2a7e4bdaa09830767eb97a48c2aecc81e83a2244
Pavlo <maks.chudin567@gmail.com>
2025-04-20 21:09:39 +0300

    refactor-product-page

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,5 +1,5 @@
 import { Cart } from '../../components/Shered/Cart/Cart';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ProductsType } from '../../types/Products';
 import { getProducts } from '../../utils/httpClient';
 import './Products.scss';
